2cadf0e979c8e61691c8e76ddbbe78e672e904ee
Afara <afarasidheek@gmail.com>
2022-07-16 12:42:59 +0400

    rightbar added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,105 @@
-
-import "./rightbar.css"
+import "./rightbar.css";
 function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
-  )
+    <div className="rightbar">
+      <div className="rightbarContainer">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+          </span>
+        </div>
+        <img src="/assets/gift.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">online friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+             <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+             <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+             <li className="rightbarFriend">
+            <div className="rightbarProfileImgcontainer">
+              <img
+                src="/assets/friend/friend.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+             
+            </div>
+            <span className="rightbarOnline"></span>
+            <span className="rightbarUserName">afara</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Rightbar
+export default Rightbar;
